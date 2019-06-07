2708334509a43d80a22c8668e3e671ae1ac058ab
Alsaddig <36203347+Alsaddig@users.noreply.github.com>
2019-06-06 22:24:46 -0700

    Add files via upload

---
+++ ibrahim-primitivetypes.js
@@ -0,0 +1,9 @@
+var  firstName= "Alsaddig";
+var lastName ="Ibrahim";
+var  age= 37;
+var  Street ="2610 Bartelt RD";
+var city= "Iowa City";
+var state ="Iowa";
+var zipCode= "52246";
+console.log('First Name: '+firstName+"\n"+'Last Name: '+lastName+"\n"+'Age: '
++age+"\n"+'Street: '+Street+"\n"+'City: '+city+"\n"+'State: '+state+"\n"+'Zip Code: '+zipCode);
